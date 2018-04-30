660c1850985b95bd9333a6778bc1cd2bb356e5b7
myra090989 <33969068+myra090989@users.noreply.github.com>
2018-04-29 20:42:09 -0700

    c

---
+++ application/views/pages/about.php
@@ -0,0 +1,12 @@
+<?php
+//application/views/pages/about.php 
+
+?>
+
+<p>Clever stuff on about page goes here</p>
+<p>Clever stuff on about page goes here</p>
+<p>Clever stuff on about page goes here</p>
+<p>Clever stuff on about page goes here</p>
+<p>Clever stuff on about page goes here</p>
+<p>Clever stuff on about page goes here</p>
+<p>Clever stuff on about page goes here</p>
---
+++ application/views/pages/home.php
@@ -0,0 +1,11 @@
+<?php
+//application/views/pages/home.php 
+
+?>
+
+<p>Clever stuff on home page goes here</p>
+<p>Clever stuff on home page goes here</p>
+<p>Clever stuff on home page goes here</p>
+<p>Clever stuff on home page goes here</p>
+<p>Clever stuff on home page goes here</p>
+<p>Clever stuff on home page goes here</p>
